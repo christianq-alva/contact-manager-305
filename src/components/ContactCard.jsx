754ab609eb5beccdc5947790cf39dd48e905b84b
Christian <christian_quinteros@outlook.com>
2025-12-03 16:35:37 -0500

    Parte 2 m+ RA

---
+++ src/components/ContactCard.jsx
@@ -0,0 +1,9 @@
+export default function ContactCard() {
+    return (
+        <div>
+            <h3>Mi primer contacto</h3>
+            <p>Teléfono: +51 994156682</p>
+            <p>Email: christianquinteros@upeu.edu.pe</p>
+        </div>
+    )
+}
---
+++ src/components/Copyright.jsx
@@ -0,0 +1,8 @@
+export default function CopyrightFullYear() {
+    return (
+        <div>
+            <h2>Christian Quinteros</h2>
+            <p>{new Date().getFullYear()}</p>
+        </div>
+    )
+}
